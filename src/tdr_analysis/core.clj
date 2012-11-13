3d6fca3ba0d467d095c157aa34463867fea0f4d8
Max Czapanskiy <maxczapanskiy@gmail.com>
2012-11-13 12:54:29 -0800

    Repository Created
    Created a project locally using lein. Used GitHub app to push to GitHub.

---
+++ src/tdr_analysis/core.clj
@@ -0,0 +1,1 @@
+(ns tdr-analysis.core)
---
+++ test/tdr_analysis/test/core.clj
@@ -0,0 +1,6 @@
+(ns tdr-analysis.test.core
+  (:use [tdr-analysis.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
