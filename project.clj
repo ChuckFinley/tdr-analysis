364041cb08ca85b7839d276ff9ae94cf5993012a
Max Czapanskiy <maxczapanskiy@gmail.com>
2012-11-13 17:20:22 -0800

    Tuple extraction
    Two fun bits here. Note that unless you specify a filename both
    functions default to the sample file 'assets/10Aug11.csv'.
    1) get-col-heads looks at the first row of a csv file and creates a
    mapping from column name (as a keyword) to column index. That way we
    can access fields in csv files by name.
    2) csv-tuples takes a vector of fields and returns a sequence of
    vectors of those fields from each row.

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject tdr-analysis "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+	:main tdr-analysis.core
+  :dependencies [[org.clojure/clojure "1.3.0"]
+								[org.clojure/data.csv "0.1.2"]
+								[clj-time "0.4.4"]])
--- src/tdr_analysis/core.clj
+++ src/tdr_analysis/core.clj
@@ -1,1 +1,39 @@
-(ns tdr-analysis.core)
+(ns tdr-analysis.core
+	(:require [clojure.data.csv :as csv])
+	(:require [clojure.java.io :as io])
+	(:require [clojure.string :as string])
+	(:require [clj-time.core :as time])
+	(:require [clj-time.format :as time]))
+
+(def sample-file "assets/10Aug11.csv")
+
+(defn get-col-heads
+	"Take the first row of a csv file (the headers), convert it to lower
+	case, map it to keywords, and create a hashmap of keyword=>index. 
+	Now can be used to index into csv fields by name."
+	([]
+	(get-col-heads sample-file))
+	([filename]
+	(with-open [csv-file (io/reader sample-file)]
+		(let [headers (.readLine csv-file)]
+			(zipmap
+				(map keyword (string/split (string/lower-case headers) #","))
+				(iterate inc 0))))))
+
+(def col-heads (get-col-heads))
+
+(defn csv-tuples
+	"fields => vector of keyword identifiers for csv fields.
+	A sequence of vectors containing a tuple for the fields given. Use
+	next to strip the tuple from the header line."
+	([fields]
+	(csv-tuples sample-file fields))
+	([filename fields]
+	(next
+		(with-open [csv-file (io/reader sample-file)]
+			(doall
+				(for [row (csv/read-csv csv-file)]
+					(apply vector
+						(for [field fields
+							:let [idx (col-heads field)]]
+							(nth row idx nil)))))))))
