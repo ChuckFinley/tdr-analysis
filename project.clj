e4202ea6f58948fad32f4fc04b830c5f7e5a138a
Max Czapanskiy <maxczapanskiy@gmail.com>
2012-11-18 23:38:30 -0800

    .gitignore - ignore output file
    10Aug11.csv - Larger sample size (note: this looks wrong in GitHub app,
    double check on website)
    
    project.clj - add dependency on clojure-contrib for the math library
    
    core.clj -
      *  Change col-heads from a map to a vector. Now instead of looking up
    where a fields is we just zipmap the field vector with the row vector.
      *  Change cleanse-csv to take a key-value vector instead of two
    arguments. This makes preserving the hash-map structure of the record
    table easier.
      *  csv-tuples is now csv-records. The records table is now a sequence
    of hash-maps. Since order within a row is no longer important, the
    field list for filtering is now a set. Other changes were made to
    create the sequence-of-hash-maps structure.
      *  normalize-datetime automatically works on datetime.
      *  slope is now velocity and it destructs a record using the datetime
    and pressure keys.
      *  seq-slope is gone
      *  motion uses pressure and velocity to determine if the bird is
    ascending, descending, on the surface, or at the bottom. Needs better
    heuristics for more accurate results.
      *  strr is recursive str. Converts all scalars inside of nested
    collections to string for writing to csv.
      *  analyze-dives expands the record table from datetime and pressure
    to those plus velocity and motion. Still need to add dive #.
      *  write-csv rejiggers the record table and spits it to a file using
    csv/write-csv.

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "FIXME: write description"
 	:main tdr-analysis.core
   :dependencies [[org.clojure/clojure "1.3.0"]
+								[org.clojure/clojure-contrib "1.2.0"]
 								[clojure-csv/clojure-csv "2.0.0-alpha1"]
 								[clj-time "0.4.4"]])
--- src/tdr_analysis/core.clj
+++ src/tdr_analysis/core.clj
@@ -3,7 +3,8 @@
 	(:require [clojure.java.io :as io])
 	(:require [clojure.string :as string])
 	(:require [clj-time.core :as time])
-	(:require [clj-time.coerce :as time.coerce]))
+	(:require [clj-time.coerce :as time.coerce])
+	(:require [clojure.contrib.math :as math]))
 
 (defn parse-csv
 	"Microsoft puts carriage returns at the end of lines in CSV files
@@ -17,61 +18,101 @@
 	case, map it to keywords, and create a hashmap of keyword=>index. 
 	Now can be used to index into csv fields by name."
 	([]
-	(get-col-heads sample-file))
+		(get-col-heads sample-file))
 	([filename]
-	(with-open [csv-file (io/reader filename)]
-		(let [headers (first (parse-csv csv-file))]
-			(zipmap
-				(map #(keyword (string/lower-case %)) headers)
-				(iterate inc 0))))))
+		(with-open [csv-file (io/reader filename)]
+			(let [headers (first (parse-csv csv-file))]
+				(map #(keyword (string/lower-case %)) headers)))))
 
 (def col-heads (get-col-heads))
 
-(defn cleanse-csv [f v]
+(defn cleanse-csv [[k v]]
 	(try
 		(cond
-			(= f :datetime)
+			(= k :datetime)
 				(let [[M D Y h m s] (map #(Integer. %) (re-seq #"\d+" v))]
-					(time.coerce/to-long (time/date-time (+ 2000 Y) M D h m s)))
-			:else (read-string v))
+					[k (time.coerce/to-long (time/date-time (+ 2000 Y) M D h m s))])
+			:else [k (read-string v)])
 		(catch Exception e nil)))
 
-(defn csv-tuples
-	"fields => vector of keyword identifiers for csv fields.
-	Output: a sequence of tuples for the fields given."
+(defn csv-records
+	"fields => a set of keyword identifiers for csv fields.
+	Output: a sequence of records for the fields given."
 	([fields]
-		(csv-tuples sample-file fields))
+		(csv-records sample-file fields))
 	([filename fields]
 		(with-open [csv-file (io/reader filename)]
 			(doall
 				(next
 					(for [row (parse-csv csv-file)
-								:let [tuple
-											(for [f fields]
-												(cleanse-csv f (row (col-heads f))))]
-								:when (every? identity tuple)]
-						(into [] tuple)))))))
+								:let [record
+											(map cleanse-csv (filter #(fields (first %)) (zipmap col-heads row)))]
+								:when (every? identity record)]
+							(into {} record)))))))
+
+(defn update
+	"applies f to value for k in h"
+	[h f k]
+	(if (contains? h k)
+		(assoc h k (f (k h)))
+		h))
 
 (defn normalize-datetime
 	"Reduce datetime to seconds since beginning of log and drop milliseconds"
 	[t]
-	(let [t0 (reduce min (map first t))]
-		(map (fn [[t p]] [(/ (- t t0) 1000) p]) t)))
+	(let [t0 (reduce min (map :datetime t))]
+		(map (fn [r] (update r #(/ (- % t0) 1000) :datetime)) t)))
 
 (def time-pres
-	(normalize-datetime (csv-tuples [:datetime :pressure])))
+	(normalize-datetime (csv-records #{:datetime :pressure})))
 
-(defn slope [[x1 y1] [x0 y0]]
+(defn velocity [{x0 :datetime y0 :pressure} {x1 :datetime y1 :pressure}]
 	(cond
 		(= x1 x0) 0.0
 		:else (/ (- y1 y0) (- x1 x0))))
 
-(defn seq-slope
-	"t => sequence of tuples e.g. ([x0 y0] [x1 y1]). Must be sorted by x.
-	Output tuples with slope (0 first slope) e.g ([x0 y0 0] [x1 y1 s1])"
-	([t]
-		(seq-slope t [0 (-> t first second)]))
-	([t xy0]
-		(if-let [xy1 (first t)]
-			(cons [(first xy1) (second xy1) (slope xy1 xy0)] (seq-slope (next t) xy1))
-			() )))
+(defn motion
+	"pressure < 0 => surf
+	 velocity > .25 => desc
+	 velocity < .25 => asc
+	 (abs velocity) < .25 AND pressure > 1 => bott"
+	([{p :pressure v :velocity}]
+		(motion p v))
+	([p v]
+		(cond
+			(< p 0.0) :surf
+			(> v 0.25) :desc
+			(< v -0.25) :asc
+			(and (< (math/abs v) 0.25) (> p 1)) :bott)))
+
+(defn strr
+	"Applies str to scalars in a collection recursively (for writing to csv)"
+	[l]
+	(if (coll? l)
+		(map strr l)
+		(str l)))
+
+(defn analyze-dives
+	"Convert {time, pressure} to {time, presure, velocity, motion, dive #}"
+	([[r0 & rn]]
+		(let [r0 (assoc r0 :velocity 0 :motion nil :dive -1)
+					r1 (first rn)
+					v1 (velocity r0 r1)
+					m1 (motion (:pressure r1) v1)]
+			(cons r0 (analyze-dives rn v1 m1 -1))))
+	([[r0 & rn] v m d]
+		(if-let [r1 (first rn)]
+			(cons
+				(assoc r0 :velocity v :motion m :dive d)
+				(let [p1 (:pressure r1)
+							v1 (velocity r0 r1)
+							m1 (motion p1 v1)]
+					(analyze-dives rn v1 m1 d)))
+			[(assoc r0 :velocity v :motion m :dive d)])))
+
+(defn write-csv
+	"Take table of records and spit it to a csv file"
+	([table file]
+		(spit file
+			(csv/write-csv
+				(cons (map name (keys (first table))) (strr (map vals table)))))))
